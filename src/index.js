23c738850c143231e0cdafc7ad0252c1d754979d
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2023-01-07 10:03:43 +0430

    update index.js
    removing some unnecessary vars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import _ from 'lodash';
 import './style.css';
-import LoadTodos, {storageKey,loadedTodos} from './modules/LoadTodos.js';
+import LoadTodos from './modules/LoadTodos.js';
 import AddTodos from './modules/Add.js';
 import CheckboxAction from './modules/Checkbox.js';
 
@@ -8,7 +8,6 @@ const todoInput = document.getElementById('todo-input-id');
 const clearAll = document.querySelector('.all-clear-btn');
 
 window.onload = () => {
-  localStorage.setItem(storageKey, JSON.stringify(loadedTodos));
   LoadTodos.todoFunc();
 };
 
